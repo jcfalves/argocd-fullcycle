e2d4031c6dd89b81536b42fe5d3262d7ca1c2004
jcfalves <josefilgueiraalves@gmail.com>
2026-08-26 17:07:01 -0400

    que comecem os jogos

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.19 AS build
+
+WORKDIR /app    
+COPY . .
+RUN CGO_ENABLED=0 go build -o server main.go
+
+FROM alpine:3.12
+WORKDIR /app
+COPY --from=build /app/server .
+EXPOSE 8090
+CMD ["./server"]
+
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+	// Your code here
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Hello, World Jose Carlos!</h1>"))
+	})
+
+	http.ListenAndServe(":8090", nil)
+}
